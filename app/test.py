6b849131703a6f361e5759fd7fbb50294c2e4306
Igor Bondarenko <bondarenko200021@mail.ru>
2026-07-06 15:37:14 +0700

    test script

---
+++ app/test.py
@@ -0,0 +1,25 @@
+import asyncssh
+import asyncio
+
+HOST = "192.168.15.65"
+USER = "admin"
+PASSWORD = "r0vgh0vfn"
+COMMAND = "info"
+
+
+async def main():
+    async with asyncssh.connect(
+        HOST,
+        username=USER,
+        password=PASSWORD,
+        known_hosts=None,
+    ) as conn:
+        result = await conn.run(COMMAND)
+        print("STDOUT - читает")
+        print(result.stdout)
+        print("STRERR")
+        print(result.stderr)
+        print("EXIT", result.exit_status)
+
+
+asyncio.run(main())
