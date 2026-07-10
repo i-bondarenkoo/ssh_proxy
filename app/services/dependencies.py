85b811f9cacb827147a18645532426f56fa504a1
Igor Bondarenko <bondarenko200021@mail.ru>
2026-07-10 11:30:53 +0700

    1

--- app/services/dependencies.py
+++ app/services/dependencies.py
@@ -5,7 +5,7 @@ def timer(func):
     async def wrapper(*args, **kwargs):
         start = time.time()
         result = await func(*args, **kwargs)
-        stop = time.time
+        stop = time.time()
         print(f"Время выполнения запроса - {stop - start} секунд")
         return result
 
