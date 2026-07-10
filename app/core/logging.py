dc44f019d68aee306fa1364e15ee5952e54ee079
Igor Bondarenko <bondarenko200021@mail.ru>
2026-07-10 12:45:55 +0700

    1

--- app/core/logging.py
+++ app/core/logging.py
@@ -1,6 +1,14 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
 logger = logging.getLogger(__name__)
 
 
-logging.basicConfig(format="%(filename)s:%(levelname)s:%(message)s", level=logging.INFO)
+logging.basicConfig(
+    format="%(filename)s:%(levelname)s:%(message)s",
+    level=logging.INFO,
+    handlers=[
+        logging.StreamHandler(),
+        RotatingFileHandler("ssh_proxy.log", maxBytes=10000000, backupCount=5),
+    ],
+)
